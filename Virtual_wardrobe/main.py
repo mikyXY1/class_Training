35cfaf1d40569289ec5c75afb7f4343b1e082736
Miky <mm1234@digitm.net>
2025-11-10 15:24:00 +0100

    uprava kodu

--- Virtual_wardrobe/main.py
+++ Virtual_wardrobe/main.py
@@ -1,3 +1,5 @@
+from data import clothing_data
+
 #vytvoreni tridy Clothing - obecny popis - DNA
 class Clothing:
     # konstruktor třídy
@@ -8,6 +10,22 @@ class Clothing:
         self.material = material
 
 # vytvoreni instance = objektu tridy Clothing - "porod"
-q_1 = Clothing("Tricko", "bile", "M", "bavlna")
-q_2 = Clothing("Kalhoty", "modre", "L", "denim")
+# q_1 = Clothing("Tricko", "bile", "M", "bavlna")
+# q_2 = Clothing("Kalhoty", "modre", "L", "denim")
 
+
+clothing_list = []
+for one_item in clothing_data:       # bere data z Dictionary = clothing_data a uklada je do Listu = clothing_list
+    name = (one_item["name"]) 
+    color = (one_item["color"])
+    size = (one_item["size"])
+    material = (one_item["material"]) 
+    new_item = Clothing(name, color, size, material)
+    clothing_list.append(new_item)
+
+print(clothing_list[0])  # vypise prvni objekt z listu clothing_list = name + color + size + material
+print(clothing_list[0].name)  # vypise jen name prvniho oblečení z listu clothing_list
+print(clothing_list[0].color)  # vypise jen color prvniho oblečení z listu clothing_list
+print(clothing_list[0].size)  # vypise jen size prvniho oblečení z listu clothing_list
+print(clothing_list[0].material)  # vypise jen material prvniho oblečení z listu clothing_list
+print(clothing_list)
