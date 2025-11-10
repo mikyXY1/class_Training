389ded17ce06cfe33b6438890319dd027ce67ac3
Miky <mm1234@digitm.net>
2025-11-10 16:06:39 +0100

    uprava kodu

--- Virtual_wardrobe/data.py
+++ Virtual_wardrobe/data.py
@@ -2,8 +2,8 @@
 
 clothing_data=[
 	{"name": "tricko", "color": "bila", "size": "M", "material": "bavlna"},
-	{"name": "kalhoty", "color": "modre", "size": "L", "material": "denim"},
+	{"name": "kalhoty", "color": "modra", "size": "L", "material": "denim"},
 	{"name": "kabat", "color": "cerna", "size": "XL", "material": "vlna"},
 	{"name": "halenka", "color": "ruzova", "size": "S", "material": "hedvabi"},
-    {"name": "kratasy", "color": "zelene", "size": "M", "material": "len"}
+    {"name": "kratasy", "color": "zelena", "size": "M", "material": "len"}
 ]
--- Virtual_wardrobe/main.py
+++ Virtual_wardrobe/main.py
@@ -9,10 +9,15 @@ class Clothing:
         self.size = size
         self.material = material
 
+    def find_color(color):   # metoda pro nalezeni barvy obleceni
+        for item in clothing_list:
+            if item.color.lower() == color.lower():
+                print(f"Nalezene obleceni: {item.name}, Barva: {item.color}, Velikost: {item.size}, Material: {item.material}")
+
+    
 # vytvoreni instance = objektu tridy Clothing - "porod"
 # q_1 = Clothing("Tricko", "bile", "M", "bavlna")
 # q_2 = Clothing("Kalhoty", "modre", "L", "denim")
-
 
 clothing_list = []
 for one_item in clothing_data:       # bere data z Dictionary = clothing_data a uklada je do Listu = clothing_list
@@ -23,9 +28,12 @@ for one_item in clothing_data:       # bere data z Dictionary = clothing_data a 
     new_item = Clothing(name, color, size, material)
     clothing_list.append(new_item)
 
-print(clothing_list[0])  # vypise prvni objekt z listu clothing_list = name + color + size + material
-print(clothing_list[0].name)  # vypise jen name prvniho oblečení z listu clothing_list
-print(clothing_list[0].color)  # vypise jen color prvniho oblečení z listu clothing_list
-print(clothing_list[0].size)  # vypise jen size prvniho oblečení z listu clothing_list
-print(clothing_list[0].material)  # vypise jen material prvniho oblečení z listu clothing_list
-print(clothing_list)
+user_answer = input(f"Jakou barvu obleceni hledate?: ")
+Clothing.find_color(user_answer)
+
+# print(clothing_list[0])  # vypise prvni objekt z listu clothing_list = name + color + size + material
+# print(clothing_list[0].name)  # vypise jen name prvniho oblečení z listu clothing_list
+# print(clothing_list[0].color)  # vypise jen color prvniho oblečení z listu clothing_list
+# print(clothing_list[0].size)  # vypise jen size prvniho oblečení z listu clothing_list
+# print(clothing_list[0].material)  # vypise jen material prvniho oblečení z listu clothing_list
+# print(clothing_list)
